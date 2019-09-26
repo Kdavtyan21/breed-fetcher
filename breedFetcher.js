04681fcd3ce4db36d16703185ce5134c8800ca37
Karen <karendavtyan21@mail.com>
2019-09-26 19:53:46 +0000

    fixed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,14 @@
 const input = process.argv.slice(2)
 const request = require('request')
-const url = 'https://api.thecatapi.com/v1/images/search?breed_id=char'
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input
 request(url, (error, res, body) => {
-  if (error) {
-    console.log('error here')
-  } else {
-  console.log(typeof body)
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  }
-})
+  const breedDescription = data[0]
+  if (error) {
+    throw error
+  } else if (breedDescription == null) {
+    console.log('error')
+  } else {
+    console.log(breedDescription.description);
+   }
+});
